fa1aefa75dcc63ac6d7d3c860f8602e6b3809f54
evgeniy gazetdinov <zplacesound@gmail.com>
2019-07-28 14:26:35 +0300

    Add files via upload

---
+++ lib/delete_handler.py
@@ -0,0 +1,30 @@
+import os
+
+#module has function for delete with specific conditions
+
+def delete_file(filename):
+    os.remove(filename)
+
+def find_free_space():
+    #values on some machine possible some devert
+    #free size in bites
+    st = os.statvfs('/')
+    free = st.f_bavail * st.f_frsize
+    return free
+
+
+def delete_handler(files, limit,middle_file,free_space = 0,):
+    # count how many files need deleted
+    quantity_for_delete = abs(int(round((limit - free_space) / middle_file)))
+    print('files need deleted '+str(quantity_for_delete))
+    delete_paths = []
+    for i in files:
+        for key, value in i.items():
+            delete_paths.append(value)
+    try:
+        for i in range(quantity_for_delete):
+            print(str(delete_paths[i]) + ' file will be deleted')
+            #delete_file(delete_paths[i])
+        print(str(len(quantity_for_delete))+'will deleted')
+    except:
+        pass
--- remover.py
+++ remover.py
@@ -2,21 +2,36 @@ import os
 
 from lib.search_videos import find_videos
 from lib.file_size import find_size
-
+from lib.older_by import find_older
+from lib.delete_handler import delete_handler,find_free_space
 _PATH = os.getcwd()
-
-
+#bite
+ratio = 1048576
+middle_file = 40*ratio
 
 
 
 
 def main():
     #each func must me for single and call with map)
+    #return list
     videos = find_videos(_PATH)
+    #return list
     files_by_hour = map(find_size,videos)
+    #return value
     total_by_hour = sum(files_by_hour)
+    #return value
     limit_for_delete = total_by_hour*3
-
+    #return list
+    older_files = map(find_older,videos)
+    free_space = find_free_space()
+    
+    print(free_space)
+    print(middle_file)
+    #default free_space is 0 for delete all
+    #func take inside list with files
+    print(older_files)
+    delete_handler(older_files,limit_for_delete,middle_file)
 
 if __name__ == "__main__":
     main()
