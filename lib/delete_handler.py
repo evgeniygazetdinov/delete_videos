39c2b3e731bc0ddf0aecb696b3a2c287daf7b788
evgeshakrasava <zplacesound@gmail.com>
2019-07-28 13:00:03 +0300

    add del handler

--- lib/delete_handler.py
+++ lib/delete_handler.py
@@ -2,6 +2,8 @@ import os
 
 #module has function for delete with specific conditions
 
+def delete_file(filename):
+    os.remove(filename)
 
 def find_free_space():
     #values on some machine possible some devert
@@ -14,3 +16,20 @@ def find_free_space():
     total_size = total_blocks * block_size / giga
     free_size = free_blocks * block_size / giga
     return (free_size * 1000 + 200)*100000
+
+
+def delete_handler(files, limit, free_space = 0):
+    # count how many files need deleted
+    quantity_for_delete = abs(int(round((limit - free_space) / middle_file)))
+    print('files need deleted '+str(quantity_for_delete))
+    delete_paths = []
+    for i in files:
+        for key, value in i.items():
+            delete_paths.append(value)
+    try:
+        for i in range(quantity_for_delete):
+            print(str(delete_paths[i]) + ' file will be deleted')
+            #delete_file(delete_paths[i])
+        print(str(len(quantity_for_delete))+'will deleted')
+    except:
+        pass
--- remover.py
+++ remover.py
@@ -2,9 +2,8 @@ import os
 
 from lib.search_videos import find_videos
 from lib.file_size import find_size
-from lib.delete_handler import find_free_space
 from lib.older_by import find_older
-from lib.delete_handler import
+from lib.delete_handler import delete_handler,find_free_space
 _PATH = os.getcwd()
 ratio = 1000000
 
@@ -20,12 +19,14 @@ def main():
     files_by_hour = map(find_size,videos)
     #return value
     total_by_hour = sum(files_by_hour)
-    limit_for_delete = total_by_hour*3
     #return value
-    free_space = find_free_space()
+    limit_for_delete = total_by_hour*3
+    #return list
     older_files = map(find_older,videos)
-    print(older_files)
-
+    free_space = find_free_space()
+    #default free_space is 0 for delete all
+    #func take inside list with files
+    delete_handler(older_files,limit_for_delete)
 
 if __name__ == "__main__":
     main()
