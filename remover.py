20acd0cc88edc880bccb7b82a961ee1d6a443b2b
evgeshakrasava <zplacesound@gmail.com>
2019-07-28 11:03:57 +0300

    first commit

---
+++ remover.py
@@ -0,0 +1,22 @@
+import os
+
+from search_videos import find_videos
+
+_PATH = os.getcwd()
+
+
+
+
+
+
+def main():
+    #each func must me for single and call with map)
+    videos = find_videos(_PATH)
+    
+
+
+
+
+
+if __name__ == "__main__":
+    main()
