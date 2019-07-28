394514bcc3675907e8807d1230a2f2cc1510c9e7
evgeniy gazetdinov <zplacesound@gmail.com>
2019-07-28 11:32:45 +0300

    refactoring fize

--- remover.py
+++ remover.py
@@ -1,6 +1,7 @@
 import os
 
-from search_videos import find_videos
+from lib.search_videos import find_videos
+from lib.file_size import find_size
 
 _PATH = os.getcwd()
 
@@ -12,10 +13,9 @@ _PATH = os.getcwd()
 def main():
     #each func must me for single and call with map)
     videos = find_videos(_PATH)
-    
-
-
-
+    files_by_hour = map(find_size,videos)
+    total_by_hour = sum(files_by_hour)
+    limit_for_delete = total_by_hour*3
 
 
 if __name__ == "__main__":
