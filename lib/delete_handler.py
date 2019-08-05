0046526564df220a3b55ee4fb52482c766c34577
evgeniy gazetdinov <zplacesound@gmail.com>
2019-08-05 15:30:46 +0300

    Update delete_handler.py

--- lib/delete_handler.py
+++ lib/delete_handler.py
@@ -1,35 +1,39 @@
 import os
+import datetime
 
 #module has function for delete with specific conditions
 
 def delete_file(filename):
     os.remove(filename)
 
-def find_free_space():
+def find_free_space(path):
     #values on some machine possible some devert
     #free size in bites
-    st = os.statvfs('/')
+    st = os.statvfs(path)
     free = st.f_bavail * st.f_frsize
     return free
 
 
-def delete_handler(files, limit,middle_file,ratio,free_space = 1,):
+def delete_handler(files, limit,middle_file,ratio,free_space = 1,path):
     # count how many files need deleted
     counter = 0
-    free_space = find_free_space()
-    print(str(len(files))+'files for delete')
-    print(free_space)
+    free_space = find_free_space(path)
     delete_paths = []
     for i in files:
         for key, value in i.items():
             delete_paths.append(value)
+    print(str(len(files))+' files for delete')
+    free_space = find_free_space(path)
     try:
-        while free_space< limit:
-            free_space = find_free_space()
-            #delete_file(files[counter])
-            print(counter)
-            counter+1
-            print(str(delete_paths[counter])+ ' file will be deleted')
+       while free_space< limit:
+            if free_space>= limit:
+                print("you have enough space "+ str(free_space/ratio)+'gb',flush = True)
+                print(datetime.datetime.now(),flush =True)
+            free_space = find_free_space(path)
+            delete_file(delete_paths[counter])
+            counter+=1
+            print(delete_paths[counter])
     except:
-         print('files'+str(counter)+'deleted')
-         pass
+        print('*'*50)
+        print('files'+str(counter)+'deleted')
+        pass
